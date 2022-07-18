6dc62f84f4addf2021d94a24d7d388f82fe86fba
sindhu188 <sindhujashukla12345@gmail.com>
2022-07-18 11:44:48 +0530

    commit and push

--- src/Pattern6.java
+++ src/Pattern6.java
@@ -1,3 +1,11 @@
+/*
+* * *  * * *
+* *      * *
+*          *
+* *      * *
+* * *   * * *
+ */
+
 import java.util.Scanner;
 
 public class Pattern6 {
@@ -8,15 +16,15 @@ public class Pattern6 {
         int sp = 1;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= st; j++){
-                System.out.print("*\t");
+                System.out.print("*\t");// print star
             }
             for(int j = 1; j <= sp; j++){
-                System.out.print("\t");
+                System.out.print("\t");//print space
             }
-            for(int j = 1; j <= st; j++){
-                System.out.print("*\t");
+            for(int j = 1; j <= st; j++){ // print star
+                System.out.print("*\t");//
             }
-            if(i <= n/2){
+            if(i <= n/2){ // if the number is less than i value then decrease the star and increase the space
                 st--;
                 sp +=2;
             } else{
